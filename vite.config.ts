639dd6aa751ed42b835a26a8b3f77601bfdf4657
Brian Duan <iceice666@outlook.com>
2026-09-22 14:44:24 +0800

    build(repo/app): scaffold vite, typescript, vitest and wrangler

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+  test: {
+    globals: true,
+    environment: "node",
+    include: ["tests/**/*.test.ts"],
+  },
+});
